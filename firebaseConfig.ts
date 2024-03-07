d3321ea4ab00d56903c7c21e3797dc5a50f06146
priyannshu08 <priyanshukumar83200@gmail.com>
2024-03-07 10:20:37 +0530

    CODE_SNIPPET CODE

---
+++ firebaseConfig.ts
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBPa1QjG5Wfc-fkKIP9m_JFkFIAOzCqQg0",
+  authDomain: "codebin-ng-yt-fc663.firebaseapp.com",
+  projectId: "codebin-ng-yt-fc663",
+  storageBucket: "codebin-ng-yt-fc663.appspot.com",
+  messagingSenderId: "620924401763",
+  appId: "1:620924401763:web:0ad2635794464914aaa056"
+};
+
+
+  
